d8c4f85825c5e846a6827e9d60ac7555bb783887
Daniel Nwogo <118936910+nigerianpickle@users.noreply.github.com>
2023-07-12 21:27:59 +0100

    Update Lesson3_UserInput.c

--- Lesson3_UserInput.c
+++ Lesson3_UserInput.c
@@ -5,12 +5,14 @@ int main(){
     int age;
 
     //Asks user for input:
-    printf("Enter input value:");
+    printf("Enter your age:");
     //this function asks for an integer and stores it in the memory of the age variable
     scanf("%d", &age);
 
 
     //Prints the age
     printf("\nThe age you entered is :%d",age);
+
+    
     return 0;
 }
